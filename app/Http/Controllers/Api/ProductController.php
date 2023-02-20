041c5ebc5d79e654a24a86c8c52770b58a725a05
Eric Heinitz <ericheinitz@gmail.com>
2023-02-19 23:28:30 -0300

    se crean las funciones de productos completas

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,32 +4,47 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     
     public function index()
     {
-        //
+        $producs = Product::all();;
+        return $producs;
     }
     
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->save();
     }
     
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+        return $product;
     }
     
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::findOrFail($request->id);
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->save();
+        return $product;
     }
     
     public function destroy($id)
     {
-        //
+        $product = Product::destroy($id);
+        return $product;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\ProductController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::middleware(['auth:sanctum'])->group(function () {
+    Route::controller(ProductController::class)->group(function () {
+        Route::get('/products', 'index');
+        Route::post('/product', 'store');
+        Route::get('/product/{id}', 'show');
+        Route::put('/product/{id}', 'update');
+        Route::delete('/product/{id}', 'destroy');
+    });
+// });
