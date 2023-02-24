0a51a681727d05f3bf673b5eb7576f7e373c35d2
Eric Heinitz <ericheinitz@gmail.com>
2023-02-24 16:52:03 -0300

    fix ; en products

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     
     public function index()
     {
-        $producs = Product::all();;
+        $producs = Product::all();
         return $producs;
     }
     
