6056bbbf2f3dc46b63c4046d4e6da29cdc4b2612
Eric Heinitz <ericheinitz@gmail.com>
2023-02-23 21:36:23 -0300

    se crean fake de products

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ProductController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // Users
+    // Route::controller(UserController::class)->group(function () {
+    //     Route::get('/users', 'index');
+    //     Route::post('/user', 'store');
+    //     Route::get('/user/{id}', 'show');
+    //     Route::put('/user/{id}', 'update');
+    //     Route::delete('/user/{id}', 'destroy');
+    // });
+
+    // Products
     Route::controller(ProductController::class)->group(function () {
         Route::get('/products', 'index');
         Route::post('/product', 'store');
@@ -17,4 +29,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
         Route::put('/product/{id}', 'update');
         Route::delete('/product/{id}', 'destroy');
     });
-// });
+});
